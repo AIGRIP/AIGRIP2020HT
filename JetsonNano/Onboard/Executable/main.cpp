d1b644512f63cab31500f458c31857204d5f8393
AIGRIP <aigripmdh@gmail.com>
2020-11-19 15:02:44 +0100

    shit almost works, i go home now Samuel will cont

--- JetsonNano/Onboard/Executable/main.cpp
+++ JetsonNano/Onboard/Executable/main.cpp
@@ -1,7 +1,18 @@
-// #include <iostream>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//extern "cBalance"
+
+//#include "testFuncHardwareGPU.h"
+#include "colourSegmentation.h"
+#include "colourBalance.h"
+#include "morphologicalFilters.h"
+
 #include <opencv2/opencv.hpp>
-// #include <opencv2/videoio.hpp>
-// #include <opencv2/highgui.hpp>
+#include <opencv2/core/mat.hpp>
+
 
 std::string gstreamer_pipeline (int capture_width, int capture_height, int display_width, int display_height, int framerate, int flip_method) {
     return "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)" + std::to_string(capture_width) + ", height=(int)" +
@@ -12,13 +23,22 @@ std::string gstreamer_pipeline (int capture_width, int capture_height, int displ
 
 int main()
 {
-    int capture_width =10 ;
-    int capture_height =10 ;
-    int display_width = 10 ;
-    int display_height = 10 ;
-    int framerate = 60 ;
-    int flip_method = 0 ;
 
+    int width = 740, height = 984;
+    bool binIm1[728160];
+    bool binIm2[728160];
+   
+
+    unsigned char resultImage[728160];
+    unsigned char colourBalancedImage[2184480];
+    unsigned char outputImg[2184480];
+
+    int capture_width = 1280 ;
+    int capture_height = 720 ;
+    int display_width = 740 ;
+    int display_height = 984 ;
+    int framerate = 60 ;
+    int flip_method = 3 ;
     std::string pipeline = gstreamer_pipeline(capture_width,
 	capture_height,
 	display_width,
@@ -32,28 +52,72 @@ int main()
 	std::cout<<"Failed to open camera."<<std::endl;
 	return (-1);
     }
-
-//    cv::namedWindow("CSI Camera", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("CSI Camera", cv::WINDOW_AUTOSIZE);
     cv::Mat img;
-
+    //cv::Mat3d imgChan;
+    std::vector<cv::Mat> channels;
     std::cout << "Hit ESC to exit" << "\n" ;
-    
-   
+    while(true)
+    {
     	if (!cap.read(img)) {
 		std::cout<<"Capture read error"<<std::endl;
+		break;
+	
+		//std::cout<<imgChan.size()<<std::endl;
+
 	}
-	
 	//cv::imshow("CSI Camera",img);
-	int keycode = cv::waitKey(30) & 0xff ;
-	cv::MatIterator_<double> _it = img.begin<double>();
-	for(;_it!=img.end<double>(); _it++){
-    	std::cout << *_it <<std:: endl;}
-	
-    
+	split(img, channels);
+	//std::cout<<"blue:"<<channels[0].size<<std::endl;
+	//std::cout<<"Green:"<<channels[1].size<<std::endl;
+	//std::cout<<"Red:"<<channels[2].size<<std::endl;
+	cv::imshow("CSI Camera",img);
+	//std::cout<<img.size<<std::endl;
+	int keycode = cv::waitKey(30) & 0xff ; 
+        if (keycode == 27) break ;
+    }
+
+//cv::Vec3b tempVar;
+//int count = 0;
+
+//for(int k=0; k<3; k++){
+//	for(int i=0; i<(img.rows); i++){
+//		for(int j = 0; j<(img.cols); j++){
+//			tempVar = img.at<cv::Vec3b>(i,j);
+//			outputImg[count] = tempVar[k];
+//			count++;
+			
+			
+//		}
+//	}
+//}
+  //    std::cout<<"outputimg"<<sizeof outputImg<<std::endl;
+      unsigned char* dataOut = img.data;
+      colourBalance(dataOut, colourBalancedImage);
+
+      colourSegmentation( colourBalancedImage,(double) round(height/2),(double) round(width/2) ,binIm1);
+
+      morphologicalFilters( binIm1,(double) round(height/2),(double) round(width/2) ,binIm2 );
+
+	for(int i=0;i<(width-5);i=i+5)
+	{
+		for(int j=0;j<(height-5);j=j+5)
+		{
+			if( binIm2[i*height + j] == 0)
+			{
+				printf(" ");
+			}else{
+				printf("1");
+			}
+		}
+		printf("\n");
+	}
 
     cap.release();
+    cv::destroyAllWindows() ;
     return 0;
-
-
-
 }
+
+
+
+
