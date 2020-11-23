f1da23ae2ff8644462c5ad707236461fa0ff7902
AIGRIP <aigripmdh@gmail.com>
2020-11-23 12:53:01 +0100

    Added bluetooth communication.

---
+++ JetsonNano/Onboard/communication/communicationBLT.c
@@ -0,0 +1,111 @@
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/rfcomm.h>
+
+#include <pthread.h>
+
+int client;
+
+void receiveBluetoothMessages(void *arg)
+{
+    int bytes_read;
+    char receiveBuffer[1024] = { 0 };
+    memset(receiveBuffer, 0, sizeof(receiveBuffer));
+
+
+    while(1)
+    {
+        // read data from the client
+        bytes_read = read(client, receiveBuffer, sizeof(receiveBuffer));
+        if( bytes_read > 0 ) {
+            printf("received [%s]\n", receiveBuffer);
+            fflush(stdout);
+        }
+
+        usleep(10000);
+    }
+}
+
+
+int main(int argc, char **argv)
+{
+    struct sockaddr_rc loc_addr = { 0 }, rem_addr = { 0 };
+    
+    char sendBuffer[1024] = { 0 };
+
+    int s, bytes_read, threadCommander;
+    socklen_t opt = sizeof(rem_addr);
+
+    // allocate socket
+    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+
+    // bind socket to port 1 of the first available 
+    // local bluetooth adapter
+    loc_addr.rc_family = AF_BLUETOOTH;
+    loc_addr.rc_bdaddr = *BDADDR_ANY; //"F8:1F:32:35:83:D9";
+    loc_addr.rc_channel = (uint8_t) 1;
+    bind(s, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
+
+    // put socket into listening mode
+    listen(s, 1);
+
+    // accept one connection
+    client = accept(s, (struct sockaddr *)&rem_addr, &opt);
+
+    ba2str( &rem_addr.rc_bdaddr, sendBuffer );
+    fprintf(stderr, "accepted connection from %s\n", sendBuffer);
+
+    pthread_attr_t attr;
+    pthread_t thread_id;
+    threadCommander = 0;
+
+    pthread_attr_init(&attr);
+    pthread_create(&thread_id, &attr,(void*) &receiveBluetoothMessages, (void*)&threadCommander);
+
+    char *StrMatch;
+    int stringLength;
+
+    while(1)
+    {
+
+
+        memset(sendBuffer, 0, sizeof(sendBuffer));
+
+        fgets(sendBuffer,1024,stdin);
+
+        stringLength =strlen(sendBuffer);
+
+        StrMatch = strchr(sendBuffer,0);
+        //*StrMatch = '\n';
+
+        StrMatch = strstr(sendBuffer,"quit");
+
+        if(StrMatch == NULL)
+        {
+            bytes_read = write(client, sendBuffer, stringLength);
+            if( bytes_read > 0 ) {
+                printf("Sent: %s", sendBuffer);
+            }else{
+                printf("Failed to send message.\n");
+            }
+        }else{
+            break;
+        }
+
+
+    }
+
+    threadCommander = 1;
+
+    pthread_cancel(thread_id);
+    //pthread_join(thread_id,NULL)
+
+    // close connection
+    close(client);
+    close(s);
+    return 0;
+}
