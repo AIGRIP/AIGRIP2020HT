4feada16baa38b70e174e0bcfb445aed4fbb18ce
AIGRIP <71131624+AIGRIP@users.noreply.github.com>
2020-12-15 14:44:17 +0100

    Update control.cpp

--- JetsonNano/Onboard/control/control.cpp
+++ JetsonNano/Onboard/control/control.cpp
@@ -9,7 +9,7 @@
 #include <mqueue.h>
 
 #include "typedefsGripperNano.h"
-#include "imageCapture.h"
+//#include "imageCapture.h"
 
 #include "colourSegmentation.h"
 #include "colourBalance.h"
@@ -23,6 +23,16 @@
 
 #include <time.h>
 #include <sys/time.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/mat.hpp>
+
+//Gstreamer multimedia framework
+std::string gstreamer_pipeline (int capture_width, int capture_height, int display_width, int display_height, int framerate, int flip_method) {
+    return "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)" + std::to_string(capture_width) + ", height=(int)" +
+           std::to_string(capture_height) + ", format=(string)NV12, framerate=(fraction)" + std::to_string(framerate) +
+           "/1 ! nvvidconv flip-method=" + std::to_string(flip_method) + " ! video/x-raw, width=(int)" + std::to_string(display_width) + ", height=(int)" +
+           std::to_string(display_height) + ", format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
+}
 
 // Main function for communication.
 void* controlThread(void* arg)
@@ -114,6 +124,34 @@ void* controlThread(void* arg)
     struct timespec messageDeadline;
 
 
+    int capture_width = 740 ;
+    int capture_height = 984 ;
+    int display_width = 740 ;
+    int display_height = 984 ;
+    int framerate = 20 ;
+    int flip_method = 1 ;
+
+    std::string pipeline = gstreamer_pipeline(capture_width, // call to gstream func
+	capture_height,
+	display_width,
+	display_height,
+	framerate,
+	flip_method);
+
+    cv::VideoCapture cap(pipeline, cv::CAP_GSTREAMER); //Opening camera
+
+
+    if(!cap.isOpened()) 
+    {
+	    std::cout<<"Failed to open camera."<<std::endl;
+    }
+
+    cv::Mat img;
+    std::vector<cv::Mat> channels;
+    cv::Vec3b tempVar;
+    int count;
+
+
     while(1)
     {
 
@@ -166,7 +204,19 @@ void* controlThread(void* arg)
             // Preshape
             case 1:
             {
-                imageCaptureFunc(outputImg);
+                cap.grab();
+                cap.retrieve(img);
+                count = 0;
+
+                for(int k=0; k<3; k++){
+                    for(int i=0; i<(img.cols); i++){
+                        for(int j = 0; j<(img.rows); j++){
+                            tempVar = img.at<cv::Vec3b>(j,i);
+                            outputImg[count] = tempVar[2-k];   //Create output image
+                            count++;
+                        }
+                    }
+                }
 
                 // Take time of colourBalance.
                 gettimeofday(&startTimeStruct, NULL);
@@ -388,6 +438,6 @@ void* controlThread(void* arg)
 	    usleep(10000);
 
     }
-
+    cap.release();
 }
 
